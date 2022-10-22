7c0b3f9b7365b4ef0130707c206fb8a11522a200
kazmonroy <katherinemonroy@gmail.com>
2022-10-23 01:07:03 +0200

    Add Hourly and Daily Forecast

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './scss/style.scss';
-import getForecast from './modules/get-forecast';
+import { getHourlyForecast, getDailyForecast } from './modules/get-forecast';
 import { createCurrentWeatherCard, createForm } from './modules/dom';
 
 import '@fortawesome/fontawesome-free/js/fontawesome';
@@ -15,6 +15,8 @@ function startApp(e) {
   createForm();
   showCurrentWeather(e);
   createCurrentWeatherCard('Malmö');
+  showHourlyForecast('Malmö');
+  showDailyForecast('Malmö');
 }
 
 function showCurrentWeather() {
@@ -22,22 +24,100 @@ function showCurrentWeather() {
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let wrapper = document.querySelector('[data-wrapper]');
+
     const weatherCard = e.target.nextSibling;
 
     const queryInput = document.querySelector('[data-input-query]');
     const cityQuery = queryInput.value;
 
-    getForecast(cityQuery);
-
     if (weatherCard === null) {
       createCurrentWeatherCard(cityQuery);
+      showHourlyForecast(cityQuery);
+      showDailyForecast(cityQuery);
     } else if (weatherCard !== null) {
       {
-        weatherCard.remove();
+        // weatherCard.remove();
+        wrapper.children[1].remove();
+        wrapper.children[2].remove();
+        wrapper.children[3].remove();
+
         createCurrentWeatherCard(cityQuery);
+        showHourlyForecast(cityQuery);
+        showDailyForecast(cityQuery);
       }
     }
 
     queryInput.value = '';
+  });
+}
+
+// function showForecast(cityQuery) {
+//   showHourlyForecast(cityQuery);
+//   showDailyForecast(cityQuery);
+// }
+
+function showHourlyForecast(cityQuery) {
+  getHourlyForecast(cityQuery).then((forecast) => {
+    const wrapper = document.querySelector('[data-wrapper]');
+
+    const forecastDisplay = document.createElement('div');
+    forecastDisplay.setAttribute('class', 'hour-forecast-display');
+
+    for (let i = 0; i < 5; i++) {
+      const hourForecastCard = document.createElement('div');
+      hourForecastCard.setAttribute('class', 'hour-forecast-card');
+
+      const hour = document.createElement('div');
+      hour.textContent = forecast[i].time;
+
+      const temp = document.createElement('div');
+      temp.textContent = `${forecast[i].temp}°`;
+
+      hourForecastCard.append(hour);
+      hourForecastCard.append(temp);
+
+      forecastDisplay.append(hourForecastCard);
+    }
+
+    wrapper.append(forecastDisplay);
+  });
+}
+
+function showDailyForecast(cityQuery) {
+  getDailyForecast(cityQuery).then((weekForecast) => {
+    const wrapper = document.querySelector('[data-wrapper]');
+    const dailyForecastDisplay = document.createElement('div');
+    dailyForecastDisplay.setAttribute('class', 'daily-forecast-display');
+    console.log(weekForecast);
+
+    for (let i = 0; i < 5; i++) {
+      const dayForecastCard = document.createElement('div');
+      dayForecastCard.setAttribute('class', 'day-forecast-card');
+
+      const day = document.createElement('div');
+      day.setAttribute('class', 'day');
+      day.textContent = weekForecast[i].day;
+
+      const icon = document.createElement('img');
+      icon.src = ` http://openweathermap.org/img/wn/${weekForecast[i].icon}.png`;
+
+      const maxTemp = document.createElement('div');
+      maxTemp.textContent = weekForecast[i].max_temp;
+      maxTemp.setAttribute('class', 'max-temp');
+
+      const minTemp = document.createElement('div');
+      minTemp.textContent = weekForecast[i].min_temp;
+      maxTemp.setAttribute('class', 'min-temp');
+
+      dayForecastCard.append(day);
+      dayForecastCard.append(icon);
+      dayForecastCard.append(maxTemp);
+      dayForecastCard.append(minTemp);
+
+      dailyForecastDisplay.append(dayForecastCard);
+    }
+
+    wrapper.append(dailyForecastDisplay);
   });
 }
--- src/modules/get-forecast.js
+++ src/modules/get-forecast.js
@@ -2,7 +2,7 @@ import getCurrentweather from './get-weather';
 
 let WEATHER_API_KEY = 'a81e03c8d54e3f2e938c5105c6e861e3';
 
-export default async function getForecast(cityQuery) {
+export async function getHourlyForecast(cityQuery) {
   try {
     let json = await getCurrentweather(cityQuery);
 
@@ -15,28 +15,167 @@ export default async function getForecast(cityQuery) {
 
     let jsonForecast = await responseForecast.json();
 
-    let listOfForecast = jsonForecast.list;
+    let forecastList = jsonForecast.list;
 
-    let rawDates = listOfForecast.map((item) => new Date(item.dt * 1000));
+    let rawDates = forecastList.map((item) => new Date(item.dt * 1000));
 
-    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    // Temperature
+    let temperature = forecastList.map((item) => {
+      let kelvinTemp = item.main.temp;
+      let kelvin = 273.15;
+      let celsiusTemp = Math.floor(kelvinTemp - kelvin) + 1;
 
+      return celsiusTemp;
+    });
+
+    // Hours
+    let hours = rawDates.map((date) => {
+      let rawHours = date.getHours();
+      let hour = rawHours > 12 ? `${rawHours - 12} pm` : `${rawHours} am`;
+
+      return hour;
+    });
+
+    let hourlyForecast = [];
+    let len = temperature.length;
+
+    for (let i = 0; i < len; i++) {
+      hourlyForecast.push({
+        time: hours[i],
+        temp: temperature[i],
+      });
+    }
+
+    // let dailyForecast = [];
+    // for (let i = 0; i < len; i++) {
+    //   dailyForecast.push({
+    //     day: days[i],
+    //     icon: icons[i],
+    //     max_temp: maxTemp[i],
+    //     min_temp: minTemp[i],
+    //   });
+    // }
+
+    return hourlyForecast;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getDailyForecast(cityQuery) {
+  try {
+    let json = await getCurrentweather(cityQuery);
+
+    let lat = json.coord.lat;
+    let lon = json.coord.lon;
+
+    let responseForecast = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    let jsonForecast = await responseForecast.json();
+
+    let forecastList = jsonForecast.list;
+
+    let rawDates = forecastList.map((item) => new Date(item.dt * 1000));
+
+    // Temperature
+    let temperature = forecastList.map((item) => {
+      let kelvinTemp = item.main.temp;
+      let kelvin = 273.15;
+      let celsiusTemp = Math.floor(kelvinTemp - kelvin) + 1;
+
+      return celsiusTemp;
+    });
+
+    let maxTemp = forecastList.map((item) => {
+      let kelvinTemp = item.main.temp_max;
+      let kelvin = 273.15;
+      let celsiusTemp = Math.floor(kelvinTemp - kelvin) + 1;
+
+      return celsiusTemp;
+    });
+
+    let minTemp = forecastList.map((item) => {
+      let kelvinTemp = item.main.temp_min;
+      let kelvin = 273.15;
+      let celsiusTemp = Math.floor(kelvinTemp - kelvin) + 1;
+
+      return celsiusTemp;
+    });
+
+    // Days
+    const daysOfWeek = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
     let dayIndex = rawDates.map((date) => date.getDay());
     let days = dayIndex.map((index) => daysOfWeek[index]);
 
-    let dates = rawDates.map((date) => date.getDate());
-    let hours = rawDates.map((date) => date.getHours());
+    // Hours
+    let hours = rawDates.map((date) => {
+      let rawHours = date.getHours();
+      let hour = rawHours > 12 ? `${rawHours - 12} pm` : `${rawHours} am`;
 
-    let forecastDates = [];
-    let len = days.length;
+      return hour;
+    });
+
+    let icons = forecastList.map((item) => {
+      return item.weather[0].icon;
+    });
+
+    let hourlyForecast = [];
+    let len = temperature.length;
 
     for (let i = 0; i < len; i++) {
-      forecastDates.push({ data: `${days[i]} ${dates[i]}` });
+      hourlyForecast.push({
+        time: hours[i],
+        temp: temperature[i],
+      });
     }
 
-    console.log(forecastDates);
+    let dailyForecast = [];
+    for (let i = 0; i < len; i++) {
+      dailyForecast.push({
+        day: days[i],
+        icon: icons[i],
+        max_temp: maxTemp[i],
+        min_temp: minTemp[i],
+      });
+    }
 
-    return jsonForecast;
+    const sun = dailyForecast.find(
+      (forecast) => forecast.day === daysOfWeek[0]
+    );
+    const mon = dailyForecast.find(
+      (forecast) => forecast.day === daysOfWeek[1]
+    );
+    const tue = dailyForecast.find(
+      (forecast) => forecast.day === daysOfWeek[2]
+    );
+    const wed = dailyForecast.find(
+      (forecast) => forecast.day === daysOfWeek[3]
+    );
+    const thu = dailyForecast.find(
+      (forecast) => forecast.day === daysOfWeek[4]
+    );
+
+    const fri = dailyForecast.find(
+      (forecast) => forecast.day === daysOfWeek[5]
+    );
+
+    const sat = dailyForecast.find(
+      (forecast) => forecast.day === daysOfWeek[6]
+    );
+
+    const weekForecast = [sun, mon, tue, wed, thu, fri, sat];
+
+    return weekForecast;
   } catch (err) {
     console.log(err);
   }
